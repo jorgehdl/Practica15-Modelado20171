8e860043cabcd9984bad4528fb1fab6347c4b3d2
jorgehdl <jorgehdl@hotmail.com>
2016-12-01 14:31:26 -0600

    Header del metodo main

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H // Guarda para evitar include circulares
+#define MAIN_H
+/* ------ Se hacen todos los includes necesarios ------ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* ------ Definición de tus estructuras ------ */
+
+
+/* ------ Definición de tus funciones ------ */
+
+/*Imprime las instrucciones de uso del programa*/
+void imprime_instrucciones();
+
+#endif //MAIN_H
